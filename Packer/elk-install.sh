68f2e33d8797fa1ae88f8254706f4bb087cbdb29
Ian Weston <iancweston@gmail.com>
2020-07-06 22:15:02 -0400

    updated elk script to fix install and removed initial updates

--- Packer/elk-install.sh
+++ Packer/elk-install.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-# Update system
-sudo apt update && sudo apt upgrade
-sudo apt install wget
-
+# Run commands with root permission
+sudo su
 
 ###########################
 # Elasticsearch
 ###########################
 
-sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-sudo apt-get install apt-transport-https
-sudo echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt-get update && sudo apt-get install elasticsearch
-sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable elasticsearch.service
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+apt-get update && apt-get install elasticsearch
+sleep 5
+/bin/systemctl daemon-reload
+/bin/systemctl enable elasticsearch.service
 
 ###########################
 # Kibana
 ###########################
 
-sudo apt-get update && sudo apt-get install kibana
-sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable kibana.service
+apt-get update && apt-get install kibana
+sleep 5
+/bin/systemctl daemon-reload
+/bin/systemctl enable kibana.service
 
 ###########################
 # Filebeat
 ###########################
 
-sudo apt-get update && sudo apt-get install filebeat
-# systemctl enable filebeat
+apt-get update && apt-get install filebeat
 
+# Return to user
+exit
