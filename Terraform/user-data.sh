fb02b7cc8ae718e8458d0844c6d55a70937a926d
Zach Paul <zachpaul.21@gmail.com>
2020-07-09 16:53:42 -0700

    Consolidated scripts into one startup script

--- Terraform/user-data.sh
+++ Terraform/user-data.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
-# Stop Kibana
-service kibana stop
+###########################
+# Update and Upgrade
+###########################
+apt-get update -y && apt-get upgrade -y
 
-# bind url to 0.0.0.0 for kibana.yml
+###########################
+# Elasticsearch
+###########################
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
+apt-get update && apt-get install elasticsearch
+/bin/systemctl daemon-reload
+/bin/systemctl enable elasticsearch.service
+service elasticsearch start
+
+###########################
+# Kibana
+###########################
+
+apt-get update && apt-get install kibana
+/bin/systemctl daemon-reload
+/bin/systemctl enable kibana.service
 line=$(echo "server.host:" \"0.0.0.0\")
 echo $line >> /etc/kibana/kibana.yml
-
-# Start Kibana
 service kibana start
+
+###########################
+# Filebeat
+###########################
+
+apt-get update && apt-get install filebeat
+
+###########################
+# Velociraptor
+###########################
+
+cd /opt
+wget https://github.com/Velocidex/velociraptor/releases/download/v0.4.5/velociraptor-v0.4.5-linux-amd64
+chmod +x velociraptor-v0.4.5-linux-amd64
