50e53c87f6370d94b63ef0ea9109d304e532f00b
Zach Paul <zachpaul.21@gmail.com>
2020-07-09 22:45:38 -0700

    added in a status file to determine when the install is complete

--- Terraform/user-data.sh
+++ Terraform/user-data.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+###########################
+# Set status of install
+###########################
+touch /home/ubuntu/Starting_Installation.txt
 
 ###########################
 # Update and Upgrade
 ###########################
+mv /home/ubuntu/Starting_Installation.txt /home/ubuntu/Update_Upgrade.txt
 apt-get update -y && apt-get upgrade -y
 
 ###########################
 # Elasticsearch
 ###########################
-
+mv /home/ubuntu/Update_Upgrade.txt /home/ubuntu/Installing_Elasticsearch.txt
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
@@ -20,7 +25,7 @@ service elasticsearch start
 ###########################
 # Kibana
 ###########################
-
+mv /home/ubuntu/Installing_Elasticsearch.txt /home/ubuntu/Installing_Kibana.txt
 apt-get update && apt-get install kibana
 /bin/systemctl daemon-reload
 /bin/systemctl enable kibana.service
@@ -29,13 +34,15 @@ service kibana start
 ###########################
 # Filebeat
 ###########################
-
+mv /home/ubuntu/Installing_Kibana.txt /home/ubuntu/Installing_Filebeat.txt
 apt-get update && apt-get install filebeat
 
 ###########################
 # Velociraptor
 ###########################
-
+mv /home/ubuntu/Installing_Filebeat.txt /home/ubuntu/Downloading_Velociraptor.txt
 cd /opt
 wget https://github.com/Velocidex/velociraptor/releases/download/v0.4.5/velociraptor-v0.4.5-linux-amd64
 chmod +x velociraptor-v0.4.5-linux-amd64
+
+mv /home/ubuntu/Downloading_Velociraptor.txt /home/ubuntu/Installation_Complete.txt
