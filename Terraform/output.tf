364ae74e702c6c34905804dc01449f60eac56ec2
Ian Weston <iancweston@gmail.com>
2020-07-09 20:57:59 -0400

    changed server-key-file output

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -4,6 +4,6 @@ output "elk-dns" {
 }
 
 output "server-key-file" {
-    value       = tls_private_key.elk-server-key.public_key_pem
+    value       = tls_private_key.elk-server-key.private_key_pem
     description = "SSH key file for ELK server"
 }
