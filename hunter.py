e7f05be3b58ac6d86cd00b120b7f5b53c5a6e534
Zach Paul <zachpaul.21@gmail.com>
2020-07-10 13:56:24 -0700

    Added argparse functionality

--- hunter.py
+++ hunter.py
@@ -1,5 +1,13 @@
 import argparse
 
+parser = argparse.ArgumentParser(description='Use this program to interact with Velociraptor and ELK Infrastructure.')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-b', '--build', action='store_true', help='Build infrastructure')
+group.add_argument( '-c', '--connect', action='store_true', help='Connect to your infrastructure')
+group.add_argument('-a', '--auth', action='store_true', help='Set Velocirpator Username and Password')
+group.add_argument('-d', '--destroy', action='store_true', help='Build infrastructure')
+args = parser.parse_args()
+
 #Set Velociraptor Username and Password (interactive)
 def set_vr_uname_pword():
     print("Set VR UNAME PWORD Function")
@@ -23,4 +31,14 @@ def destroy_infra():
     print("Destroy infra function")
     return
 
-
+if __name__ == "__main__":
+    if args.build:
+        deploy_infra()
+    elif args.connect:
+        connect_infra()
+    elif args.auth:
+        set_vr_uname_pword()
+    elif args.destroy:
+        destroy_infra()
+    else:
+        print(parser.print_help())
