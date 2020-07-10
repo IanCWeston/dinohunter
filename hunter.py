02d9de7d05edf3537ab3e5526a3535c56f861374
Zach Paul <zachpaul.21@gmail.com>
2020-07-10 11:31:02 -0700

    Added the bones to a hunter.py

---
+++ hunter.py
@@ -0,0 +1,26 @@
+import argparse
+
+#Set Velociraptor Username and Password (interactive)
+def set_vr_uname_pword():
+    print("Set VR UNAME PWORD Function")
+    return
+
+
+#Deploy infrastructure 
+def deploy_infra():
+    print("Deploy infra Function")
+    return
+
+
+#Connect to infrastructure
+def connect_infra():
+    print("connect to infra Function")
+    return
+
+
+#Destroy infrastructure
+def destroy_infra():
+    print("Destroy infra function")
+    return
+
+
