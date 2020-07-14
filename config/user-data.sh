3a65e5332ac8212db9a4b8fa25b3fd319ebd0fcc
Ian Weston <iancweston@gmail.com>
2020-07-14 10:52:11 -0400

    added formatting to date

--- config/user-data.sh
+++ config/user-data.sh
@@ -7,13 +7,13 @@ touch /home/ubuntu/elk-vr-install.log
 ###########################
 # Update and Upgrade
 ###########################
-echo "$(date): starting install" >> /home/ubuntu/elk-vr-install.log
+echo "$(date +'%b %d %T'): starting install" >> /home/ubuntu/elk-vr-install.log
 apt-get update -y && apt-get upgrade -y
 
 ###########################
 # Elasticsearch
 ###########################
-echo "$(date): installing  Elasticsearch" >> /home/ubuntu/elk-vr-install.log
+echo "$(date +'%b %d %T'): installing  Elasticsearch" >> /home/ubuntu/elk-vr-install.log
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
@@ -25,7 +25,7 @@ service elasticsearch start
 ###########################
 # Kibana
 ###########################
-echo "$(date): installing Kibana" >> /home/ubuntu/elk-vr-install.log
+echo "$(date +'%b %d %T'): installing Kibana" >> /home/ubuntu/elk-vr-install.log
 apt-get update && apt-get install kibana
 /bin/systemctl daemon-reload
 /bin/systemctl enable kibana.service
@@ -34,13 +34,13 @@ service kibana start
 ###########################
 # Filebeat
 ###########################
-echo "$(date): installing Filebeat" >> /home/ubuntu/elk-vr-install.log
+echo "$(date +'%b %d %T'): installing Filebeat" >> /home/ubuntu/elk-vr-install.log
 apt-get update && apt-get install filebeat
 
 ###########################
 # Velociraptor
 ###########################
-echo "$(date): installing Velociraptor" >> /home/ubuntu/elk-vr-install.log
+echo "$(date +'%b %d %T'): installing Velociraptor" >> /home/ubuntu/elk-vr-install.log
 cd /opt
 wget https://github.com/Velocidex/velociraptor/releases/download/v0.4.5/velociraptor-v0.4.5-linux-amd64
 chmod +x velociraptor-v0.4.5-linux-amd64
@@ -48,4 +48,4 @@ chmod +x velociraptor-v0.4.5-linux-amd64
 /opt/velociraptor-v0.4.5-linux-amd64 --config server.config.yaml user add admin --role=administrator admin
 /opt/velociraptor-v0.4.5-linux-amd64 --config /opt/server.config.yaml frontend &
 
-echo "$(date): instalation complete: your server is now ready for use" >> /home/ubuntu/elk-vr-install.log
+echo "$(date +'%b %d %T'): instalation complete: your server is now ready for use" >> /home/ubuntu/elk-vr-install.log
