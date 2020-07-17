a2aed30d770adc19c47199e767befcea7d7603a4
Ian Weston <iancweston@gmail.com>
2020-07-16 21:58:20 -0400

    removed private key output

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,9 +2,3 @@ output "dh-dns" {
     value       = aws_eip.dh_ip.public_dns
     description = "Public DNS of DinoHunter-Server"
 }
-
-output "server-key-file" {
-    value       = tls_private_key.elk-server-key.private_key_pem
-    description = "SSH key for ELK server"
-    sensitive   = true
-}
